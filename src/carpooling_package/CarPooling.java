72e643256df53b9a71440ade2778ad775afa60fb
Ahmed AbdelGawad Farag <ahmed.abdelgawad.farag@gmail.com>
2020-06-04 23:59:51 +0200

    added classes defination

--- src/carpooling_package/CarPooling.java
+++ src/carpooling_package/CarPooling.java
@@ -1,11 +1,89 @@
 package carpooling_package;
 
-public class CarPooling {
+import java.util.Scanner;
+
+class route {
+	private String startDest;
+	private String EndDest;
+
+	public route(String Start, String End) {
+		this.startDest = Start;
+		this.EndDest = End;
+	}
+
+	public void setStartDest(String startDest) {
+		this.startDest = startDest;
+	}
+
+	public void setEndDest(String endDest) {
+		EndDest = endDest;
+	}
+
+	public String getStartDest() {
+		return startDest;
+	}
+
+	public String getEndDest() {
+		return EndDest;
+	}
+
+}
+
+class Car {
+	final String Codenumber, driverName;
+	final int capacity;
+	int TripsPerDay;
+
+	public Car(String Codenumber, int capicaty, String driverName, int capacity, int TripsPerDay) {
+		this.Codenumber = Codenumber;
+		this.capacity = capacity;
+		this.driverName = driverName;
+		this.TripsPerDay = TripsPerDay;
+	}
+}
+
+class Ride {
+	route carRoute;
+	Car car;
+
+}
+
+class Ticket {
+	private double price;
+
+	public Ticket(double price) {
+
+	}
+
+}
+
+abstract class passenger {
+	Ticket t;
+	Ride ride;
+	public passenger() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+class nonSubscriber extends passenger{
 	
+	
+}
+
+class subscriber extends passenger{
+	
+	
+}
+
+
+public class CarPooling {
+
 	public static void main(String[] args) {
 
 		System.out.println("hello world");
-		
+		System.out.println("hello world");
+
 	}
 
 }
